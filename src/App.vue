e9e08309e17c14c61c610e1d8b665d634c24c97b
dong <1565629610@qq.com>
2021-06-15 23:00:50 +0800

    frist commit 2021

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+   vue组件
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+// ESLint 问题解决
+
+module.exports = {
+    // lintOnSave: false, // 关闭ESLint的规则检查
+    lintOnSave: 'warning', // 输出提示错误, 但项目继续运行
+}
